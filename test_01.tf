b3e043a01774b2f6ae22bb593634701b62c246ff
tigelane <tige@ignw.io>
2020-02-11 16:19:06 -0800

    commit testing on a branch

---
+++ test_01.tf
@@ -0,0 +1,17 @@
+resource "aci_bridge_domain" "bridge_domain" {
+  tenant_dn               = "${aci_tenant.tenant.id}"
+  name                    = "192.168.2_V200"
+  relation_fv_rs_ctx      = "${aci_vrf.vrf.name}"
+}
+
+resource "aci_subnet" "subnet" {
+  bridge_domain_dn        = "${aci_bridge_domain.bridge_domain.id}"
+  ip                      = "192.168.2.1/24"
+}
+
+resource "aci_application_epg" "epg" {
+  application_profile_dn  = "${aci_application_profile.app_profile.id}"
+  name                    = "${aci_bridge_domain.bridge_domain.name}"
+  description             = "I was built for the some servers!"
+  relation_fv_rs_bd       = "${aci_bridge_domain.bridge_domain.name}"
+}
