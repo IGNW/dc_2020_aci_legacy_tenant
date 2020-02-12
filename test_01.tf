ae2fd511b4aac31b94173066ccf2d1eebe5b6916
tigelane <tige@ignw.io>
2020-02-12 06:38:16 -0800

    adding to the names

--- test_01.tf
+++ test_01.tf
@@ -1,15 +1,15 @@
-resource "aci_bridge_domain" "bridge_domain" {
+resource "aci_bridge_domain" "bridge_domain_192.168.2_V200" {
   tenant_dn               = "${aci_tenant.tenant.id}"
   name                    = "192.168.2_V200"
   relation_fv_rs_ctx      = "${aci_vrf.vrf.name}"
 }
 
-resource "aci_subnet" "subnet" {
+resource "aci_subnet" "subnet_${aci_bridge_domain.bridge_domain.name}" {
   bridge_domain_dn        = "${aci_bridge_domain.bridge_domain.id}"
   ip                      = "192.168.2.1/24"
 }
 
-resource "aci_application_epg" "epg" {
+resource "aci_application_epg" "epg_${aci_bridge_domain.bridge_domain.name}" {
   application_profile_dn  = "${aci_application_profile.app_profile.id}"
   name                    = "${aci_bridge_domain.bridge_domain.name}"
   description             = "I was built for the some servers!"
