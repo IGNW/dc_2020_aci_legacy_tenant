9983865ecbde79284cfe31616ea4991bfaad3e5e
tigelane <tige@ignw.io>
2020-02-12 06:42:36 -0800

    static resource names

--- test_01.tf
+++ test_01.tf
@@ -4,12 +4,12 @@ resource "aci_bridge_domain" "bridge_domain_192.168.2_V200" {
   relation_fv_rs_ctx      = "${aci_vrf.vrf.name}"
 }
 
-resource "aci_subnet" "subnet_${aci_bridge_domain.bridge_domain.name}" {
+resource "aci_subnet" "subnet_192.168.2_V200" {
   bridge_domain_dn        = "${aci_bridge_domain.bridge_domain.id}"
   ip                      = "192.168.2.1/24"
 }
 
-resource "aci_application_epg" "epg_${aci_bridge_domain.bridge_domain.name}" {
+resource "aci_application_epg" "epg_192.168.2_V200" {
   application_profile_dn  = "${aci_application_profile.app_profile.id}"
   name                    = "${aci_bridge_domain.bridge_domain.name}"
   description             = "I was built for the some servers!"
