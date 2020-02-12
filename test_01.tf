c5bcdfa3f78e8fe348aa00deba32b0e1b40d04da
tigelane <tige@ignw.io>
2020-02-12 06:49:12 -0800

    removing . as they are not allowed in name

--- test_01.tf
+++ test_01.tf
@@ -1,17 +1,17 @@
-resource "aci_bridge_domain" "bridge_domain_192.168.2_V200" {
+resource "aci_bridge_domain" "bridge_domain_192_168_2_V200" {
   tenant_dn               = "${aci_tenant.tenant.id}"
-  name                    = "192.168.2_V200"
   relation_fv_rs_ctx      = "${aci_vrf.vrf.name}"
+  name                    = "192.168.2_V200"
 }
 
-resource "aci_subnet" "subnet_192.168.2_V200" {
-  bridge_domain_dn        = "${aci_bridge_domain.bridge_domain.id}"
+resource "aci_subnet" "subnet_192_168_2_V200" {
+  bridge_domain_dn        = "${aci_bridge_domain.bridge_domain_192_168_2_V200.id}"
   ip                      = "192.168.2.1/24"
 }
 
-resource "aci_application_epg" "epg_192.168.2_V200" {
+resource "aci_application_epg" "epg_192_168_2_V200" {
   application_profile_dn  = "${aci_application_profile.app_profile.id}"
-  name                    = "${aci_bridge_domain.bridge_domain.name}"
+  name                    = "${aci_bridge_domain.bridge_domain_192_168_2_V200.name}"
   description             = "I was built for the some servers!"
-  relation_fv_rs_bd       = "${aci_bridge_domain.bridge_domain.name}"
+  relation_fv_rs_bd       = "${aci_bridge_domain.bridge_domain_192_168_2_V200.name}"
 }
